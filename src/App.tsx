9163d695c2a7f4a129f15b46c885775f89c4acdd
saeed <saeednajafimedia@gmail.com>
2022-10-24 20:00:35 +0330

    fix: disable serializable check, add suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { routes } from './router';
@@ -6,17 +6,19 @@ import { routes } from './router';
 const App: React.FC = () => {
   return (
     <div className='App'>
-      <Routes>
-        {routes.map((route) => {
-          return (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<route.component />}
-            />
-          );
-        })}
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={<route.component />}
+              />
+            );
+          })}
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/redux/store/store.tsx
+++ src/redux/store/store.tsx
@@ -16,6 +16,10 @@ export const store = configureStore({
   reducer: {
     user: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
