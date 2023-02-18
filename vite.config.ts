0f38448e6fa63c7e24ecd7e130459b05d4105fef
basiiil <reihaneh.dastres@gmail.com>
2023-02-18 16:14:28 +0330

    add pwa plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// import { VitePWA } from 'vite-plugin-pwa';
+// import { createPWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // VitePWA(pwaOptions),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [],
+      // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'pvchannel',
+        short_name: 'pvchannel',
+        description: 'chat platform',
+        theme_color: '#ffffff',
+        icons: [
+          // {
+          //   src: '/public/',
+          //   sizes: '192x192',
+          //   type: 'image/png',
+          // },
+        ],
+        start_url: '/',
+        lang: 'en-US',
+        orientation: 'portrait-primary',
+      },
+    }),
+  ],
 });
