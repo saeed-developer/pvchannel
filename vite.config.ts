260fb10085c04035df480c1b52bc6440786b553b
SaeedN <saeednajafimedia@gmail.com>
2023-04-03 03:42:43 +0330

    Revert "add pwa plugin for vite"
    This reverts commit 0f38448e6fa63c7e24ecd7e130459b05d4105fef.

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { VitePWA } from 'vite-plugin-pwa';
-// import { createPWA } from 'vite-plugin-pwa';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // VitePWA(pwaOptions),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: [],
-      // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'pvchannel',
-        short_name: 'pvchannel',
-        description: 'chat platform',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/chat.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-        start_url: '/',
-        lang: 'en-US',
-        orientation: 'portrait-primary',
-      },
-    }),
-  ],
+  plugins: [react()],
 });
